4824f142da1ac5fa7a6f9c6b93e3ce2a0d70300c
issamedine <b.issamedine@gmail.com>
2020-03-09 11:50:32 +0100

    implement the front

---
+++ client/src/components/ContactList.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+import AddContact from "./AddContact";
+
+class ContactList extends Component {
+  state = {
+    contactList: []
+  };
+
+  componentDidMount() {
+    this.getContacts();
+  }
+
+  getContacts = () => {
+    axios
+      .get("/contact-list")
+      .then(res => this.setState({ contactList: res.data }))
+      .then(res => console.log("res", res));
+  };
+
+  deleteContact = id => {
+    axios.delete(`/delete_contact/${id}`).then(this.getContacts());
+  };
+
+  render() {
+    console.log(this.state.contactList);
+    return (
+      <div>
+        <div>
+          {this.state.contactList.map(el => (
+            <div>
+              <p>{el.name}</p>
+              <Link to={`/edit-contact/${el._id}`}>
+                <Button
+                  type="submit"
+                  className="mt10"
+                  variant="outline-dark"
+                  // onClick={() => this.props.getPerson(el)}
+                >
+                  Edit
+                </Button>
+              </Link>
+
+              <Button
+                type="submit"
+                className="mt10"
+                variant="outline-dark"
+                onClick={() => this.deleteContact(el._id)}
+              >
+                Delete
+              </Button>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+export default ContactList;
